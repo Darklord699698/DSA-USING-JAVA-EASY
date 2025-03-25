3980e27fffe99669977ebe035a6820b58a5d59e7
UJJWAL <ujjwal8851114020@gmail.com>
2025-03-25 21:21:32 +0530

    using bitwise AND & to check for LSB (1) 1  and add it in count and then right to check the element one by one and then return the count

---
+++ NUMBEROF1BITSeasy.java
@@ -0,0 +1,10 @@
+class NUMBEROF1BITSeasy {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            count +=(n&1);
+            n=n>>>1;
+        }
+        return count;
+    }
+}
