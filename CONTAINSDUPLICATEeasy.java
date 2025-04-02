5227ca95cf5eb0a0f171369c6bd56bfe0a8eafd8
UJJWAL <ujjwal8851114020@gmail.com>
2025-04-03 02:11:26 +0530

    done

---
+++ CONTAINSDUPLICATEeasy.java
@@ -0,0 +1,11 @@
+class CONTAINSDUPLICATEeasy {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
